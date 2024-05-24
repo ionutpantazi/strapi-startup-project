8f581bf48c4732e045454995968ed0943e17e019
ionut <ionut@vocala.co>
2024-05-24 16:29:17 +0100

    use s3 bucket

--- config/plugins.js
+++ config/plugins.js
@@ -11,30 +11,30 @@ module.exports = () => {
       gql: {
         navigationItemRelated: ['Page']
       },
-      // upload: {
-      //   config: {
-      //     provider: 'aws-s3',
-      //     providerOptions: {
-      //       s3Options: {
-      //         credentials: {
-      //           accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      //           secretAccessKey: process.env.AWS_ACCESS_SECRET,
-      //         },
-      //         region: process.env.AWS_REGION,
-      //         params: {
-      //           ACL: 'public-read',
-      //           signedUrlExpires: 15 * 60,
-      //           Bucket: process.env.AWS_BUCKET,
-      //         },
-      //       },
-      //     },
-      //     actionOptions: {
-      //       upload: {},
-      //       uploadStream: {},
-      //       delete: {},
-      //     },
-      //   },
-      // }
+      upload: {
+        config: {
+          provider: 'aws-s3',
+          providerOptions: {
+            s3Options: {
+              credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+                secretAccessKey: process.env.AWS_ACCESS_SECRET,
+              },
+              region: process.env.AWS_REGION,
+              params: {
+                ACL: 'public-read',
+                signedUrlExpires: 15 * 60,
+                Bucket: process.env.AWS_BUCKET,
+              },
+            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      }
     }
   }
   
