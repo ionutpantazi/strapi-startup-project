c01260575d18a44e374f243819451ba3b600b65f
ionutpantazi <ionut28p@yahoo.com>
2024-05-28 11:48:38 +0100

    local strapi

--- config/plugins.js
+++ config/plugins.js
@@ -10,25 +10,7 @@ module.exports = () => {
       },
       gql: {
         navigationItemRelated: ['Page']
-      },
-      upload: {
-        config: {
-          provider: 'strapi-provider-ydaws-s3',
-          providerOptions: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_ACCESS_SECRET,
-            region: process.env.AWS_REGION,
-            params: {
-              Bucket: process.env.AWS_BUCKET,
-            },
-          },
-          actionOptions: {
-            upload: {},
-            uploadStream: {},
-            delete: {},
-          },
-        },
-      },
+      }
     }
   }
   
